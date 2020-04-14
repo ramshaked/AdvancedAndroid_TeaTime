10c8b20f9475eed750816e59815a655ca7fbcbef
ramshaked <ramsha7@gmail.com>
2020-04-14 13:58:24 +0300

    fixed build. Added a test: clickIncrementButton

--- app/src/androidTest/java/com/example/android/teatime/OrderActivityBasicTest.java
+++ app/src/androidTest/java/com/example/android/teatime/OrderActivityBasicTest.java
@@ -19,6 +19,9 @@ package com.example.android.teatime;
 
 import static android.support.test.espresso.Espresso.onView;
 import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.action.ViewActions.replaceText;
+import static android.support.test.espresso.action.ViewActions.typeText;
+import static android.support.test.espresso.action.ViewActions.typeTextIntoFocusedView;
 import static android.support.test.espresso.assertion.ViewAssertions.matches;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
 import static android.support.test.espresso.matcher.ViewMatchers.withText;
@@ -51,14 +54,28 @@ public class OrderActivityBasicTest {
             new ActivityTestRule<>(OrderActivity.class);
 
     @Test
+    public void clickIncrementButton_ChangesQuantityAndCost() {
+        // Click on decrement button
+        onView((withId(R.id.increment_button))).perform(click());
+
+        // Verify that the increment button incremented the quantity by 1
+        onView(withId(R.id.quantity_text_view)).check(matches(withText("1")));
+
+        // Verify that the increment button also increases the total cost to $5.00
+        onView(withId(R.id.cost_text_view)).check(matches(withText("$5.00")));
+    }
+
+    @Test
     public void clickDecrementButton_ChangesQuantityAndCost() {
 
         // Check the initial quantity variable is zero
         onView((withId(R.id.quantity_text_view))).check(matches(withText("0")));
 
+        // Me: try to set a new value (failed to run the test)
+        //onView((withId(R.id.quantity_text_view))).perform(typeTextIntoFocusedView("2"));
+
         // Click on decrement button
-        onView((withId(R.id.decrement_button)))
-                .perform(click());
+        onView((withId(R.id.decrement_button))).perform(click());
 
         // Verify that the decrement button decreases the quantity by 1
         onView(withId(R.id.quantity_text_view)).check(matches(withText("0")));
